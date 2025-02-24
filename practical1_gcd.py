c007a42b806b97faf8d42d95ec00e85e76ff44b4
Hitesh Jethava <hitesh.jethava@simformsolutions.com>
2025-02-24 11:10:40 +0530

    practical1_gcd file create

---
+++ practical1_gcd.py
@@ -0,0 +1,33 @@
+"""
+1. Write a program for computing GCD of 2 numbers with optimal data structures and less time-consuming.
+
+    The program should take input from console or args and should handle unexpected inputs    
+
+    Constraints:
+
+        - For loop is not allowed
+
+        - input should be in words:
+
+            - e.g.: onetwo = 12, sixone = 61
+
+        - words will be within zero to nine
+
+        - Cannot use inbuilt methods like max, min, or any math function    
+
+    Example 1:
+
+        - Input 1: onezero
+
+        - Input 2: twozero
+
+        - Output: onezero
+
+    Example 2:
+
+        - Input 1: twosix
+
+        - Input 2: twofour
+
+        - Output: two
+"""
