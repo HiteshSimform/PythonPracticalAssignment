a730a224881cda363250ea8bbfb74e8d0f23c34e
Hitesh Jethava <hitesh.jethava@simformsolutions.com>
2025-02-28 16:17:18 +0530

    pyc delete

--- number_conversion_project/number_conversion/gcd.py
+++ number_conversion_project/number_conversion/gcd.py
@@ -6,18 +6,18 @@ class GCDComputer:
     """
 
     @staticmethod
-    def compute(number1: int, number2: int) -> int:
+    def compute(num1: int, num2: int) -> int:
         """
         Computes the GCD of two numbers using recursion.
 
         Args:
-            number1 (int): The first integer.
-            number2 (int): The second integer.
+            num1 (int): The first integer.
+            num2 (int): The second integer.
 
         Returns:
             int: The GCD of the input integers.
         """
-        if number2 == 0:
-            return number1
+        if num2 == 0:
+            return num1
         else:
-            return GCDComputer.compute(number2, number1 % number2)
+            return GCDComputer.compute(num2, num1 % num2)
